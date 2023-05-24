290cace79fa1fab2c421c777ff986a695b7e49f4
chkim <chkim@dtwocorp.com>
2023-05-24 14:04:28 +0900

    commit test2

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// commit test
 Route::get('/', function ()
 {
     return view('welcome');
